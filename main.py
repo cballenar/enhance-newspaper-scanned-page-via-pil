8c3c134df0a8321799e7f45403e5dcff6c560248
Carlos Ballena <cballenar@gmail.com>
2022-06-22 07:09:30 -0400

    Combined all scripts and cleaned up. The script now: - reads a txt file with a list of paths of all the samples to process - For each image:   - checks the image exists   - applies high contrast enhancement   - reads orientation from high contrast version   - rotates original and high contrast version   - saves and closes both versions

---
+++ main.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
+from pytesseract import Output, image_to_osd
+import os.path
+
+source_dir = "samples" # samples/1914/09/06/002-00.jpg
+output_dir = "output"
+
+# Profile 5: High Contrast
+def enhance_high_contrast(image):
+    image = ImageOps.grayscale(image)
+    image = ImageOps.autocontrast(image, (25,60))
+    image = image.filter(ImageFilter.GaussianBlur(1))
+    image = image.filter(ImageFilter.UnsharpMask(2,150,3))
+    return image
+
+# Profile 6: Readable
+def enhance_readable(image):
+    image = ImageEnhance.Color(image).enhance(0.2)
+    image = ImageOps.autocontrast(image, (10,30))
+    image = image.filter(ImageFilter.GaussianBlur(1))
+    image = image.filter(ImageFilter.UnsharpMask(2,150,3))
+    return image
+
+# Get list of images
+# This can be gathered via `find -f . > image-sources.txt` and a cleanup
+image_sources_file = open('image-sources.txt', 'r')
+image_sources = image_sources_file.readlines()
+  
+count = 0
+# Strips the newline character. Add `[:10]` to only do a sample
+for image_source in image_sources:
+    count += 1
+    image_path_parts = image_source.split('/')
+    image_path_source = os.path.join(source_dir,image_source.strip())
+    image_exists = os.path.exists(image_path_source)
+    if (image_exists):
+        print("[INFO] Processing {}: {}".format(count, image_path_source))
+        image = Image.open(image_path_source)
+        # build output path and directory
+        image_path_output = os.path.join(output_dir,image_source.strip())
+        dir_path_output = os.path.join(output_dir,*image_path_parts[:-1])
+        os.makedirs(dir_path_output, exist_ok=True)
+        # enhance and save image
+        high_contrast_image = enhance_high_contrast(image)
+        high_contrast_image.save(image_path_output)
+        # reopen image and read osd
+        # this re-opening of the new image from path is currently necessary as osd continues to fail when opening from PIL.
+        # See: https://stackoverflow.com/questions/54047116/getting-an-error-when-using-the-image-to-osd-method-with-pytesseract
+        osd_results = image_to_osd(image_path_output, output_type=Output.DICT)
+        print("[INFO] Orientation: {} with a {} confidence.".format(osd_results["orientation"],osd_results["orientation_conf"]))
+        # If rotation seems good, apply and resave
+        if (osd_results["orientation_conf"]>0.75):
+            # apply to high contrast image
+            high_contrast_image = high_contrast_image.rotate(osd_results["orientation"], expand=1)
+            high_contrast_image.save(image_path_output)
+            # apply to original
+            image = image.rotate(osd_results["orientation"], expand=1)
+        high_contrast_image.close()
+        # enhance original for readability and close
+        image = enhance_readable(image)
+        image.save(image_path_source)
+        image.close()
+    else:
+        print("[ERROR] Line {}: {} could NOT be found.".format(count, image_path_source))
