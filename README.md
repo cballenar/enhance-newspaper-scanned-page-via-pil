cc98bdefd5b2a9d5f48036c99f5c58894a9fffd9
Carlos Ballena <cballenar@gmail.com>
2022-06-24 07:44:51 -0400

    cleaned up and improved documentation

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # Newspaper Scan Processing
+
 Python script to enhance scanned pages from newspaper archive.
+
+![Demo Image](https://github.com/cballenar/enhance-newspaper-scanned-page-via-pil/blob/main/demo.jpg?raw=true)
+
+## What it Does
 
 The script reads a txt file with a list of paths of all the samples to process and for each image:
 
 - Checks the image exists
-- Applies high contrast enhancement
-- Reads orientation from high contrast version
-- Rotates original and high contrast version
-- Saves and closes both versions
+- Applies high contrast enhancement for OCR
+- Reads orientation from OCR version
+- Rotates, both, original and OCR version
+- Exports Image (text) and Image Data (text + coordinates + confidence) to a txt and json file next to the output image
+- Applies a human readable enhancement to the original image
+- Saves and closes along with the text files
+
+## Usage
+This script assumes some basic knowledge of python and execution of scripts from command line.
+
+1. Install python requirements
+1. Add images to process to the `./source` directory.
+1. Generate a list of your image paths inside source folder.
+    - This can be achieved via `find -f . > image-sources.txt`
+    - It will need to be cleaned up to remove unwanted files
+1. If appropriate, generate a list of words and convert them to a dawg file and place it in `./training-data/user.lstm-word-dawg`.
+    - See [this for documentation](https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc) and [this for a tutorial](https://vovaprivalov.medium.com/tesseract-ocr-tips-custom-dictionary-to-improve-ocr-d2b9cd17850b).
+1. Ensure the language in the script is what you need. Defaults to Spanish.
+1. Run the script `python main.py`.
+1. Results will be printed as it progresses. All the images and read texts will be exported to the `./output` directory mirroring the path structure in source.
+
+### Script Variables
+- `source_dir` - Defaults to `./source`.
+- `output_dir` - Defaults to `./output`. 
+- `sources_index_path` - Defaults to `sources-index.txt`.
+- `user_words_path` - Defualts to `./training-data/user.lstm-word-dawg`
+- `ocr_language` - Defaults to `spa`
--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from pytesseract import Output, image_to_osd, image_to_data, image_to_string
 import os.path
 import json
 
-source_dir = "samples" # samples/1914/09/06/002-00.jpg
-output_dir = "output"
+source_dir = "source" # will be joined with the path in the sources index file
+output_dir = "output" # will mirror the paths in the sources index file
 
+sources_index_path = "sources-index.txt"
 user_words_path = "./training-data/user.lstm-word-dawg"
+ocr_language = "spa"
 
-# Profile 5: High Contrast
+# Profile: Machine Readable - High Contrast
 def enhance_high_contrast(image):
     image = image.filter(ImageFilter.UnsharpMask(15,200,18))
     image = ImageOps.autocontrast(image, (17,81))
     image = ImageOps.grayscale(image)
     return image
 
-# Profile 6: Readable
+# Profile: Human Readable
 def enhance_readable(image):
     image = image.filter(ImageFilter.GaussianBlur(1))
     image = image.filter(ImageFilter.UnsharpMask(2,150,3))
@@ -25,19 +27,20 @@ def enhance_readable(image):
 
 # Get list of images
 # This can be gathered via `find -f . > image-sources.txt` and a cleanup
-image_sources_file = open('image-sources.txt', 'r')
-image_sources = image_sources_file.readlines()
+# TODO: Program gathering of index by searching for files in the source directory
+sources_index_file = open(sources_index_path, 'r')
+sources_index = sources_index_file.readlines()
   
 count = 0
-# Strips the newline character. Add `[:10]` to only do a sample
-for image_source in image_sources:
+# Strips the newline character
+for image_source in sources_index:
     count += 1
     image_path_parts = image_source.split('/')
     image_path_source = os.path.join(source_dir,image_source.strip())
     image_file_name = image_path_parts[-1]
     image_exists = os.path.exists(image_path_source)
     if (image_exists):
-        print("[INFO] Processing {}: {}".format(count, image_path_source))
+        print("[INFO] Image {}: {}".format(count, image_path_source))
         image = Image.open(image_path_source)
         # build output path and directory
         image_path_output = os.path.join(output_dir,image_source.strip())
@@ -47,13 +50,14 @@ for image_source in image_sources:
         high_contrast_image = enhance_high_contrast(image)
         high_contrast_image.save(image_path_output)
         #
-        # reopen image and read osd
+        # ! To fix: Reopen image and read osd
         # this re-opening of the new image from path is currently necessary as osd continues to fail when opening from PIL.
         # See: https://stackoverflow.com/questions/54047116/getting-an-error-when-using-the-image-to-osd-method-with-pytesseract
         osd_results = image_to_osd(image_path_output, output_type=Output.DICT)
         print("[INFO] Orientation: {} with a {} confidence.".format(osd_results["orientation"],osd_results["orientation_conf"]))
         # If rotation seems good, apply and resave
         if (osd_results["orientation_conf"]>0.75):
+            print("[INFO] Rotating...")
             # apply to high contrast image
             high_contrast_image = high_contrast_image.rotate(osd_results["orientation"], expand=1)
             high_contrast_image.save(image_path_output)
@@ -64,23 +68,24 @@ for image_source in image_sources:
         tess_config = '--user-words {}'.format(user_words_path) if user_words_path else ''
         #
         # read string and data from image
-        print("[INFO] Reading string from file")
+        print("[INFO] Reading string from file...")
         image_string_file = open(os.path.join(dir_path_output,image_file_name[:-4]+"txt"), "w")
-        image_string_file.write(image_to_string(high_contrast_image, lang='spa', config=tess_config))
+        image_string_file.write(image_to_string(high_contrast_image, lang=ocr_language, config=tess_config))
         image_string_file.close()
         #
         # read string and data from image
-        print("[INFO] Reading data from file")
+        print("[INFO] Reading data from file...")
         image_data_file = open(os.path.join(dir_path_output,image_file_name[:-4]+"json"), "w")
-        image_data_file.write(json.dumps(image_to_data(high_contrast_image, lang='spa', output_type=Output.DICT, config=tess_config)))
+        image_data_file.write(json.dumps(image_to_data(high_contrast_image, lang=ocr_language, output_type=Output.DICT, config=tess_config)))
         image_data_file.close()
         # cleanup
         high_contrast_image.close()
         #
         # enhance original for readability, save as new output and close
-        print("[INFO] Enhancing original image")
+        print("[INFO] Enhancing original image...")
         image = enhance_readable(image)
         image.save(image_path_output)
         image.close()
+        print("[INFO] Done.\n")
     else:
-        print("[ERROR] Line {}: {} could NOT be found.".format(count, image_path_source))
+        print("[ERROR] Image {}: {} could NOT be found.\n".format(count, image_path_source))
