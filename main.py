5a6aa9b5a4971619f623c1182b450ef3c40d9eed
Carlos Ballena <cballenar@gmail.com>
2022-06-30 06:56:03 -0400

    moved rotation to its own def

--- main.py
+++ main.py
@@ -39,6 +39,34 @@ def enhance_readable(image):
     image = ImageEnhance.Color(image).enhance(0.2)
     return image
 
+# takes an enhanced image, detects its rotation and returns it
+# optionally a source image can be passed to also be rotated and returned
+# if no path is specified a tmp file will be created
+def detect_and_rotate_image(enhanced_image, output_path=os.path.join(output_dir,"tmp.jpg"), source_image=None):
+    enhanced_image.save(output_path)
+    try:
+        # ! To fix: Reopen image and read osd
+        # this re-opening of the new image from path is currently necessary as osd continues to fail when opening from PIL.
+        # See: https://stackoverflow.com/questions/54047116/getting-an-error-when-using-the-image-to-osd-method-with-pytesseract
+        osd_results = image_to_osd(output_path, output_type=Output.DICT)
+    except Exception as e:
+        logging.error(str(e))
+    else:
+        logging.info("Orientation: {} with a {} confidence.".format(osd_results["orientation"],osd_results["orientation_conf"]))
+        # If rotation seems good, apply and resave
+        if (osd_results["orientation_conf"]>0.75):
+            logging.info("Rotating...")
+            # apply to high contrast image
+            enhanced_image = enhanced_image.rotate(osd_results["orientation"], expand=1)
+            # save again if requested
+            if not do_make_human_readable:
+                enhanced_image.save(output_path)
+            # apply to original if available
+            if source_image:
+                source_image = source_image.rotate(osd_results["orientation"], expand=1)
+                return enhanced_image, source_image
+    return enhanced_image, source_image
+
 # Process a Single Image from its path
 def process_image(image_path):
     image_path_parts = image_path.split('/')
@@ -54,27 +82,10 @@ def process_image(image_path):
         os.makedirs(dir_path_output, exist_ok=True)
         # enhance and save image
         high_contrast_image = enhance_high_contrast(image)
-        high_contrast_image.save(image_path_output)
-        #
-        # Perform Rotation if requested
+        # 
+        # perform rotation if requested
         if do_rotate:
-            try:
-                # ! To fix: Reopen image and read osd
-                # this re-opening of the new image from path is currently necessary as osd continues to fail when opening from PIL.
-                # See: https://stackoverflow.com/questions/54047116/getting-an-error-when-using-the-image-to-osd-method-with-pytesseract
-                osd_results = image_to_osd(image_path_output, output_type=Output.DICT)
-            except Exception as e:
-                logging.error(str(e))
-            else:
-                logging.info("Orientation: {} with a {} confidence.".format(osd_results["orientation"],osd_results["orientation_conf"]))
-                # If rotation seems good, apply and resave
-                if (osd_results["orientation_conf"]>0.75):
-                    logging.info("Rotating...")
-                    # apply to high contrast image
-                    high_contrast_image = high_contrast_image.rotate(osd_results["orientation"], expand=1)
-                    high_contrast_image.save(image_path_output)
-                    # apply to original
-                    image = image.rotate(osd_results["orientation"], expand=1)
+            high_contrast_image, image = detect_and_rotate_image(high_contrast_image, image_path_output, image)
         #
         # read string and data from image
         if do_text_extraction:
@@ -106,13 +117,11 @@ def process_image(image_path):
 # Get list of images
 # This can be gathered via `find -f . > image-sources.txt` and a cleanup
 # TODO: Program gathering of index by searching for files in the source directory
-sources_index_file = open(sources_index_path, 'r')
-sources_index = sources_index_file.readlines()
-
 logging.info("Starting a new batch...")
-
-count = 0
-# Strips the newline character
-for sources_index_line in sources_index:
-    count += 1
-    process_image(sources_index_line.strip())
+with open(sources_index_path, 'r') as sources_index_file:
+    sources_index = sources_index_file.readlines()
+    count = 0
+    # Strips the newline character
+    for sources_index_line in sources_index:
+        count += 1
+        process_image(sources_index_line.strip())
